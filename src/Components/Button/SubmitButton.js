046adbbdeecd0b90f19887f731c7de3f4a859191
Malik Inkatha <minkatha01@gmail.com>
2017-05-12 16:19:25 -0500

    gitignore update

--- src/Components/Button/SubmitButton.js
+++ src/Components/Button/SubmitButton.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SubmitButton = ({ onClick, className = '', children }) =>
+const SubmitButton = ({ onClick, className, children }) =>
   <button 
     onClick={onClick} 
     className={className} 
@@ -11,9 +11,13 @@ const SubmitButton = ({ onClick, className = '', children }) =>
   </button>
 
 SubmitButton.PropTypes = {
-    onClick: PropTypes.func,
+    onClick: PropTypes.func.isRequired,
     className: PropTypes.string,
-    children: PropTypes.node
+    children: PropTypes.node.isRequired
+};
+
+SubmitButton.defaultProps = {
+  className: '',
 };
 
 export default SubmitButton;
